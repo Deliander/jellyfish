d1df9c3623879dbe50e2dacbe630997b6ded1756
Deliander <72639023+Deliander@users.noreply.github.com>
2023-04-14 21:55:31 +1000

    Update build-macos.sh

--- build-macos.sh
+++ build-macos.sh
@@ -1,13 +1,11 @@
 echo "[macOS] Building"
-VERSION="$(node ./getVersion.js)"
-mkdir builds
-#electron-packager . Jellyfish-BETA --exclude=docs --app-version "$VERSION" --protocol=jellyfish-lsef --platform darwin --out ./builds --overwrite --icon ../jellyfish-ui/assets/icon-beta.icns --asar --app-copyright "(c) 2020 theLMGN - Do not redistribute. Provided with NO warranty" --app-bundle-id com.thelmgn.jellyfish --darwin-dark-mode-support
-electron-packager . Jellyfish --exclude=docs --app-version "$VERSION" --protocol=jellyfish-lsef --platform darwin --out ./builds --overwrite --icon ../jellyfish-ui/assets/icon.icns --asar --app-copyright "(c) 2020 theLMGN - Do not redistribute. Provided with NO warranty" --app-bundle-id com.thelmgn.jellyfish --darwin-dark-mode-support
-cd ./builds
-mv Jellyfish-darwin-x64 "Jellyfish-$VERSION"
-cd "Jellyfish-$VERSION"
-
-#zip -r9 "Jellyfish-$VERSION.zip" Jellyfish.app
+VERSION=$(node ./getVersion.js)
+mkdir -p builds
+electron-packager . Jellyfish --exclude=docs --app-version "$VERSION" --protocol=jellyfish-lsef --platform darwin --out ./builds --overwrite --icon ../jellyfish-ui/assets/icon.icns --asar --app-bundle-id com.thelmgn.jellyfish --darwin-dark-mode-support
+cd builds/Jellyfish-darwin-x64
+mv Jellyfish.app "Jellyfish-$VERSION.app"
+cd ..
+zip -r9 "Jellyfish-$VERSION.zip" "Jellyfish-$VERSION.app"
 mv "Jellyfish-$VERSION.zip" ../
-cd ../..
-#rm -rf "builds"
+cd ..
+rm -rf "builds"
